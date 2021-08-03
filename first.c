7cb573716bf17a00ebaa7811d4d7cf4d839e691c
NokibulAlam <nokibul.alam02@gmail.com>
2021-08-04 01:25:08 +0600

    second commit

--- first.c
+++ first.c
@@ -3,4 +3,5 @@
 int main(){
     printf("First");
     printf("First changes");
+    printf("First file changes 2");
 }
--- second.c
+++ second.c
@@ -2,5 +2,5 @@
 
 int main(){
     printf("Second");
-    printf("First change");
+    int a,b;
 }
